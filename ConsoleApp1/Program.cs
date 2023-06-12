e4b8704e1c812ca3f30a1909e48dfd48e5836a53
SEOKSONG SONG <expert190@outlook.kr>
2023-06-12 10:17:00 +0900

    초기 커밋.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using IoCContainer;
+using System.ComponentModel.Design;
+
+namespace ConsoleApp1
+{
+    public class Service
+    {
+
+    }
+    public class Service2
+    {
+        public Service _service;
+        public Service2(Service service)
+        {
+            _service = service;
+        }
+    }
+    internal class Program
+    {
+        static Container container = new Container();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            container.RegisterService<Service2>((container) =>
+            {
+                var service = container.GetService<Service>();
+                return new Service2(service);
+            });
+            container.RegisterService<Service>();
+
+            var service1 = container.GetService<Service2>();
+
+            Console.WriteLine(service1._service.GetHashCode());
+            Console.WriteLine(container.GetService<Service>().GetHashCode());
+        }
+    }
+}
---
+++ IoCContainer/Container.cs
@@ -0,0 +1,37 @@
+﻿namespace IoCContainer
+{
+    public class Container
+    {
+        Dictionary<Type, Func<Container, object>> services = new Dictionary<Type, Func<Container, object>>();
+        Dictionary<Type, object> instances = new Dictionary<Type, object>();
+        public void RegisterService<T>()
+        {
+            services.Add(typeof(T), null);
+        }
+        public void RegisterService<T>(Func<Container, T> action)
+        {
+            services.Add(typeof(T), (Func<Container, object>)(object)action);
+        }
+        public T GetService<T>()
+        {
+            var srv = services.Where(x => x.Key.Name == typeof(T).Name)
+                              .FirstOrDefault();
+            var t = srv.Key;
+
+
+            if (instances.TryGetValue(t, out var instance))
+                return (T)instance;
+
+            else
+            {
+                object obj = null;
+                if (typeof(T).GetConstructor(Type.EmptyTypes) != null)
+                    obj = (T)Activator.CreateInstance(t);
+                else
+                    obj = srv.Value.Invoke(this);
+                instances.Add(t, obj);
+                return (T)instances[t];
+            }
+        }
+    }
+}
